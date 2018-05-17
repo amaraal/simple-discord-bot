9ac508c60fe44008f73dd7bc52576dcf7372d87d
PedrolinoV2 <pearrudas@gmail.com>
2018-05-17 12:36:28 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,5 +1,6 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
+const token = 'NDM3Nzg2MzMzNjc5OTc2NDQ4.DcEhmg.WjurPxHZL9W9QFdEJd_sSMMCWF0';
 
 bot.on('ready', () => {
     console.log(`Logged in as ${bot.user.tag}!`);
@@ -9,6 +10,9 @@ bot.on('message', msg =>{
     if (msg.content === 'ping') {
         msg.reply('pong');
     }
+    if (msg.content === 'qual meu avatar seu lixo?') {
+        msg.reply(msg.author.avatarURL);
+    }
 });
 
-bot.login("NDM3Nzg2MzMzNjc5OTc2NDQ4.DcEhmg.WjurPxHZL9W9QFdEJd_sSMMCWF0");
+bot.login(token);
