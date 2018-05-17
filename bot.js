08582bc67595f173cc8260aeca3524390a12e5a2
PedrolinoV2 <pearrudas@gmail.com>
2018-05-16 22:02:56 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -29,6 +29,12 @@ var evil = schedule.scheduleJob('0 3 * * *', function(){
 bot.on('message', function (user, userID, channelID, message, evt) {
     // Our bot needs to know if it will execute a command
     // It will listen for messages that will start with `!`
+    if (message.contains("TOP")){
+        bot.sendMessage({
+            to: channelID,
+            message: ':ok_hand:'
+        });
+    }
     if (message.substring(0, 1) == '!') {
         var args = message.substring(1).split(' ');
         var cmd = args[0];
