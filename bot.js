5822fb5d3aff977c1e2498d8e07aa53666ea7846
PedrolinoV2 <pearrudas@gmail.com>
2018-05-17 13:31:05 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,5 +1,6 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
+const { Client, MessageEmbed } = require('discord.js');
 const token = 'NDM3Nzg2MzMzNjc5OTc2NDQ4.DcEhmg.WjurPxHZL9W9QFdEJd_sSMMCWF0';
 
 bot.on('ready', () => {
