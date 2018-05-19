daa6116fb4f0ff45dd9c6658a35f771c720f4616
PedrolinoV2 <pearrudas@gmail.com>
2018-05-19 19:41:42 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,38 +1,73 @@
 const fs = require("fs");
 const Discord = require("discord.js");
-
-const { prefix, token, ownerID } = require("./config.json");
-
+client.on("ready", () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+  client.user.setPresence({ game: { name: `Say 👌help | I am in ${client.guilds.size} guilds! ` }, status: "online"});
+});
 const client = new Discord.Client();
 client.commands = new Discord.Collection();
-
+const { token } = require("./config.json");
 const commandFiles = fs.readdirSync("./commands");
 //var schedule = require("node-schedule");
-
 //if(message.author.id !== config.ownerID) return; <--- if only i can do the command.
-
 const Enmap = require("enmap");
 const EnmapLevel = require("enmap-level");
 const settings = new Enmap({provider: new EnmapLevel({name:"settings"})});
 
+const defaultSettings = {
+  prefix: "👌",
+  modRole: "Mod",
+  adminRole: "Admin",
+  welcomeChannel: "welcome",
+  welcomeMessage: "{{user}} has joined the server! :D",
+  goodbyeMessage: "{{user}} has left the server! ;-;"
+};
+
+client.on("guildCreate", guild => {
+  console.log(`I joined a guild called ${guild.name}, it has ${guild.memberCount} members.`);
+  client.user.setPresence({ game: { name: `Say 👌help | I am in ${client.guilds.size} guilds! ` }, status: "online"});
+  settings.set(guild.id, defaultSettings);
+});
+
+client.on("guildDelete", guild => {
+  console.log(`I have been removed from the guild ${guild.name} which had ${guild.memberCount} members.`);
+  client.user.setPresence({ game: { name: `Say 👌help | I am in ${client.guilds.size} guilds! ` }, status: "online"});
+  settings.delete(guild.id);
+});
+
+client.on("guildMemberAdd", member => {
+  const guildConf = settings.get(member.guild.id);
+  const welcomeMessage = guildConf.welcomeMessage.replace("{{user}}", member.user.tag);
+  const channel = member.guild.channels.find("name", "member-log");
+  if (!channel) return;
+  member.guild.channels.find("name", guildConf.welcomeChannel).send(welcomeMessage).catch(console.error);
+});
+
+client.on("guildMemberRemove", member => {
+  const channel = member.guild.channels.find("name", "member-log");
+  const guildConf = client.settings.get(member.guild.id);
+  const goodbyeMessage = guildConf.goodbyeMessage.replace("{{user}}", member.user.tag);
+  if (!channel) return;
+  member.guild.channels.find("name", guildConf.welcomeChannel).send(goodbyeMessage).catch(console.error);
+});
 
 for (const file of commandFiles) {
   const command = require(`./commands/${file}`);
   client.commands.set(command.name, command);
 }
 
-client.on("ready", () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-  client.user.setPresence({ game: { name: `Say 👌help | I am in ${client.guilds.size} guilds! ` }, status: "online"});
-});
-
 client.on("message", message =>{
-  if (!message.content.startsWith(prefix) || message.author.bot) return;
+  if(!message.guild || message.author.bot) return;
 
-  const args = message.content.slice(prefix.length).split(/ +/);
+  const guildConf = settings.get(message.guild.id);
+
+  if (!message.content.startsWith(guildConf.prefix)) return;
+
+  const args = message.content.slice(guildConf.prefix.length).split(/ +/);
   const commandName = args.shift().toLowerCase();
 
   const command = client.commands.get(commandName);
+
 
   if (command.guildOnly && message.channel.type != "text") {
     return message.reply("This command doesn't work here.");
@@ -42,7 +77,7 @@ client.on("message", message =>{
     let reply = `You didn't give arguments, ${message.author}!`;
 
     if (command.usage){
-      reply += `\nThe correct usage would be: \n${prefix}${commandName} ${command.usage}`;
+      reply += `\nThe correct usage would be: \n${guildConf.prefix}${commandName} ${command.usage}`;
     }
 
     return message.channel.send(reply);
@@ -57,30 +92,6 @@ client.on("message", message =>{
     console.error(error);
     message.reply("there was an error while running this command!");
   }
-});
-
-client.on("guildMemberAdd", member => {
-  const channel = member.guild.channels.find("name", "member-log");
-  if (!channel) return;
-
-  channel.send(`Welcome to the server ${member}`);
-});
-
-client.on("guildDelete", guild => {
-  console.log(`I have been removed from the guild ${guild.name} which had ${guild.memberCount} members.`);
-  client.user.setPresence({ game: { name: `Say 👌help | I am in ${client.guilds.size} guilds! ` }, status: "online"});
-});
-
-client.on("guildCreate", guild => {
-  console.log(`I joined a guild called ${guild.name}, it has ${guild.memberCount} members.`);
-  client.user.setPresence({ game: { name: `Say 👌help | I am in ${client.guilds.size} guilds! ` }, status: "online"});
-});
-
-client.on("guildMemberRemove", member => {
-  const channel = member.guild.channels.find("name", "member-log");
-  if (!channel) return;
-
-  channel.send(`${member} has left the server ;-;`);
 });
 
 client.login(token);
