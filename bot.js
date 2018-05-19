d9d43a6ac11a745d649366fca38cd20880bfc6c9
Pedro-PC <pearrudas@gmail.com>
2018-05-19 20:58:15 -0300

    bot.js

--- bot.js
+++ bot.js
@@ -59,7 +59,7 @@ for (const file of commandFiles) {
 client.on("message", message =>{
   if(message.author.bot) return;
 
-  const guildConf = settings.get(message.channel.guild.id);
+  const guildConf = settings.get(message.guild.id);
 
   if (!message.content.startsWith(guildConf.prefix)) return;
 
