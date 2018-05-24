05813ef5ebafc81dd93cbddc6d2954bb85a87d0b
Pedro-PC <pearrudas@gmail.com>
2018-05-23 22:21:10 -0300

    AAAAAAAAAAAA

--- commands/cutekitty.js
+++ commands/cutekitty.js
@@ -7,7 +7,10 @@ module.exports = {
     guildOnly: false,
     ownerOnly: false,
     async execute(message, args) {
-        const { body } = await snekfetch.get('https://aws.random.cat/meow');
+        const { body } = await snekfetch.get('https://aws.random.cat/meow').catch((err) => {
+			console.log(err);
+			message.reply("there was an error running this command.");
+		});
 
         message.channel.send(body.file);
     },
